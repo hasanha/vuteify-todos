91ee0ccee7e6537bd57d00efd84f83354659b769
Hasan Hammadeh <hasan-ha@ba-tech.tech>
2024-02-29 16:29:22 +0300

    first commit

---
+++ src/views/TodoView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <v-list 
+    flat
+    class="pt-0"
+    >
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Wake up",
+        },
+        {
+          id: 2,
+          title: "Buy phone",
+        },
+        {
+          id: 3,
+          title: "Go to work",
+        },
+      ],
+    };
+  },
+};
+</script>
