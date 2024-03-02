f5ade64ef2624a3f9b1bbb7d218942ad71cbb2c3
Hamzerh22345 <hamzeh.HAZ22345@gmail.com>
2024-03-02 12:47:57 +0300

    Add about section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about pa-6">
-    <h1>2024 Feb copyrights</h1>
+    <pre>
+<h1>Welcome to our Todo Manager!</h1>
+At BA Tech, we're passionate about productivity and efficiency. Our todo website is designed to help you organize your tasks, prioritize
+your work, and achieve your goals with ease.
+Whether you're a busy professional juggling multiple projects, a student managing assignments, or simply someone who likes to stay
+organized, our todo manager is here to simplify your life.
+
+<b><i>Key features of our todo website include:</i></b>
+
+User-Friendly Interface: Our intuitive interface makes it easy to add, edit, and prioritize tasks. 
+With just a few clicks, you can create a todo list tailored to your needs.
+1- Task Management: Organize your tasks into categories, set due dates, and mark them as completed once you're done.
+   Our website keeps track of your progress, so you can stay on top of your commitments.
+2- Collaboration: Need to collaborate with others on a project? Our website allows you to share todo lists with team
+   members, assign tasks, and track everyone's contributions in real-time.
+3- Customization: Personalize your todo lists with color-coded labels, tags, and categories. Whether you prefer a minimalist
+   layout or a more detailed view, our website can adapt to your preferences.
+4- Accessibility: Access your todo lists from anywhere, anytime. Our website is fully responsive, so you can manage your tasks
+   on the go from your desktop, laptop, or mobile device.
+
+At BA Tech, we're committed to providing innovative solutions that streamline your workflow and enhance your productivity.
+Join thousands of users who trust our todo manager to keep them organized and focused on what matters most.
+
+Start managing your tasks efficiently today with BA Tech's Todo Manager!
+    </pre>
   </div>
 </template>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,50 +1,44 @@
 <template>
   <div class="home">
-    <v-text-field
-    class="pa-3"
-    v-model="newTaskTitle"
-            outlined
-            label="Add Task"
-            append-icon="mdi-plus"
-            hide-details
-            clearable
-            @click:append="addTask"
-            @keyup.enter="addTask"
-          ></v-text-field>
-          <div v-if="tasks.length">
+    <v-text-field class="pa-3" v-model="newTaskTitle" outlined label="Add Task" append-icon="mdi-plus" hide-details
+      clearable @click:append="addTask" @keyup.enter="addTask"></v-text-field>
+    <div v-if="tasks.length">
 
-            <v-list flat class="pt-0">
-              
-              <div v-for="task in tasks" :key="task.id">
-        <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5': task.done }">
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-            </v-list-item-action>
+      <v-list flat class="pt-0">
 
-            <v-list-item-content>
-              <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-action>
-            <v-btn 
-            icon
-            @click.stop="deleteTask(task.id)"
-            >
-              <v-icon color="error">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
+        <div v-for="task in tasks" :key="task.id">
+          <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5': task.done }">
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title }}
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon @click.stop="deleteTask(task.id)">
+                  <v-icon color="error">mdi-delete</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+          <v-divider></v-divider>
+        </div>
+      </v-list>
+    </div>
+    <div v-else class="container">
+      <div class="second">
+        
+          <v-icon color="blue" x-large>
+            mdi-check
+          </v-icon>
+        
+        <div>
+          <h2>All Tasks completed</h2>
+        </div>
       </div>
-    </v-list>
-  </div>
-    <div v-else>
-      <v-icon color="blue" large>
-        mdi-check
-      </v-icon>
-      <h3>All Tasks completed</h3>
     </div>
   </div>
 </template>
@@ -54,7 +48,7 @@ export default {
   name: "Home",
   data() {
     return {
-      newTaskTitle:'',
+      newTaskTitle: '',
       tasks: [
         // {
         //   id: 1,
@@ -75,22 +69,45 @@ export default {
     };
   },
   methods: {
-    addTask(){
+    addTask() {
       let newTask = {
         id: Date.now(),
         title: this.newTaskTitle,
-        done:false,
+        done: false,
       }
       this.tasks.push(newTask);
-      this.newTaskTitle=''
+      this.newTaskTitle = ''
     },
     doneTask(id) {
       let task = this.tasks.filter(task => task.id === id)[0]
       task.done = !task.done
     },
     deleteTask(id) {
-      this.tasks = this.tasks.filter(task => task.id!== id)
+      this.tasks = this.tasks.filter(task => task.id !== id)
     }
   }
 };
 </script>
+
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+width: 100%;
+height: 600px;
+display: flex;
+justify-content: center;
+
+
+  .second{
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
